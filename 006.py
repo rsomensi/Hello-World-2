65a5d3de8420c629f9874bd769cf2f19bfe5285f
Rafael Somensi <rsomensi@gmail.com>
2020-05-28 21:56:30 -0300

    Double, triple and square root

--- 006.py
+++ 006.py
@@ -1,4 +1,4 @@
 #create an algorithm of read a number and return your double, triple and square root
 n = float(input('Insert a number: '))
-print('The double of {} is {}.\nThe triple of {} is {}.\nThe square root of {} is {}'.format(n, n * 2, n, n * 3, n, n ** (1/2)))
+print('The double of {} is {}.\nThe triple of {} is {}.\nThe square root of {} is {:.2f}'.format(n, n * 2, n, n * 3, n, n ** (1/2)))
 
