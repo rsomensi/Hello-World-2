6a05179024d00ff8f21fa5cc7cf2c22c5447cca9
Rafael Somensi <rsomensi@gmail.com>
2020-05-28 21:46:35 -0300

    Predecessor and successor

---
+++ 005.py
@@ -0,0 +1,5 @@
+#Input a number and create a program that shows your predecesor and successor
+
+n = float(input('Input a number: '))
+print("The predecessor of {} is {}.\nThe successor of {} is {}.".format(n, n - 1, n, n + 1))
+
