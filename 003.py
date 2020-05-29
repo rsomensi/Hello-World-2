e52638043a2a9031980529acebfa6f3f44c22e9e
Rafael Somensi <rsomensi@gmail.com>
2020-05-28 21:20:51 -0300

    Sum of two numbers

---
+++ 003.py
@@ -0,0 +1,8 @@
+#sum two numbers and return the result
+
+n1 = float(input('Insert the first number: '))
+n2 = float(input('Insert the second number: '))
+result = n1 + n2
+
+print('{} plus {} is equal to {}'.format(n1, n2, result))
+
