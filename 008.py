24a32aa6976b023e051d84d515b92894019f01ed
Rafael Somensi <rsomensi@gmail.com>
2020-05-28 22:23:24 -0300

    Convert meters in CM and MM

---
+++ 008.py
@@ -0,0 +1,3 @@
+#Convert meters, cm and mm
+m = float(input('How many meters you wish convert to cm and mm: '))
+print('{:.2f} meters is equal to {:.2f} CM.\n{:.2f} meters is equal to {:.2f} MM.'.format(m, m * 100, m, m * 1000))
